bd7371c4fec1edbff51be6c5df3cd8c2510509b6
DSoutter <duncan.soutter@gmail.com>
2021-08-29 15:06:02 +0100

    working radio button

--- js/app.js
+++ js/app.js
@@ -20,21 +20,25 @@ const createFilmEntry = function (form) {
     const filmListItem = document.createElement('li')
 
     const filmTitle = document.createElement('h2')
-    filmTitle.textContent = form.filmtitle.value
+    filmTitle.textContent = "Film Title: " + form.filmtitle.value
     filmListItem.appendChild(filmTitle)
 
     const director = document.createElement('h2')
-    director.textContent = form.director.value
+    director.textContent = "Director: " + form.director.value
     filmListItem.appendChild(director)
 
-    const releaseYear = document.createElement('h3')
-    releaseYear.textContent = form.releaseyear.value
+    const releaseYear = document.createElement('h4')
+    releaseYear.textContent = "Release Year: " + form.releaseyear.value
     filmListItem.appendChild(releaseYear)
 
     const watchedRecently = document.createElement('h4')
-    watchedRecently.textContent = form.watchedrecently.value
-    filmListItem.appendChild(watchedRecently)
-
+    if (form.watchedrecently.checked === true){
+      watchedRecently.textContent = "Watched Recently? " + form.watchedrecently.value
+  } else {
+    watchedRecently.textContent = "Watched Recently? No"
+  }
+  filmListItem.appendChild(watchedRecently)
+  
     return filmListItem
 }
 
